641c7571803cd794c30d70ada1a48463794d9811
Kristian Koivisto-Kokko <pinkkis@gmail.com>
2019-03-26 00:33:11 +0000

    adding npm itch.io build and building prod for release

---
+++ tasks/createZip.js
@@ -0,0 +1,39 @@
+// require modules
+const fs = require('fs');
+const archiver = require('archiver');
+const version = require('../package.json').version;
+
+// create a file to stream archive data to.
+if (!fs.existsSync('./build/')) {
+	fs.mkdirSync('./build/');
+}
+
+const output = fs.createWriteStream(`./build/release-${version}.zip`);
+const archive = archiver('zip', {
+	zlib: { level: 7 } // Sets the compression level.
+});
+
+// good practice to catch warnings (ie stat failures and other non-blocking errors)
+archive.on('warning', function (err) {
+	if (err.code === 'ENOENT') {
+		// log warning
+	} else {
+		// throw error
+		throw err;
+	}
+});
+
+// good practice to catch this error explicitly
+archive.on('error', function (err) {
+	throw err;
+});
+
+// pipe archive data to the file
+archive.pipe(output);
+
+// append files from a glob pattern
+archive.glob('**/*', {cwd: 'docs'});
+
+// finalize the archive (ie we are done appending files but streams have to finish yet)
+// 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+archive.finalize();
