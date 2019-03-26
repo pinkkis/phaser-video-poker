280a02c7e4b74e0842f30fa6dc2c70c4f49e99cd
Kristian Koivisto-Kokko <pinkkis@gmail.com>
2019-03-26 20:55:06 +0000

    remove cname from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -82,10 +82,6 @@ module.exports = {
 				from: './assets/',
 				to: './assets/'
 			},
-			{
-				from: './src/CNAME',
-				to: './'
-			}
 		], {}),
 		new MiniCssExtractPlugin({
 			filename: '[name].css',
